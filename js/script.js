ae2f6d4da7335ff375e37e5d7ab903e41f6d97a5
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-28 17:04:49 +0100

    refactor script.js

--- js/script.js
+++ js/script.js
@@ -1,55 +1,65 @@
 // VARIABLES
 
-// Selectors
+// DOM selectors declaration
 const header = document.querySelector('#header')
 const main = document.querySelector('#main')
 const footer = document.querySelector('#footer')
 const postsContainer = document.querySelector('#postsContainer')
 
-// All api posts array
-// let postsFromApi = []
+// Api fetching variables declaration
+const postsFetchUrl = 'https://lanciweb.github.io/demo/api/pictures/'
+let postData = null
+let postId = null
+let postTitle = null
+let postDate = null
+let postUrl = null
 
 // FUNCTIONS
 
 /**
  * Function that generates cards upon data collected by AJAX calls
  * @constructor
+ * @param {string} url // Url for fetch method to get data from 
+ * @param {string} container // Container where posts cards need to be entered 
  */
-function addElementsToContainerFromApi() {
-    
-}
+function addCardsToContainerFromApi(url, container) {
+    // API call to fetch all posts and enter them in a variable
+    fetch(url)
 
-// APIs
+        // It converts data in json when fetched
+        .then(response => response.json())
 
-// API call to fetch all posts and enter them in a variable
-fetch('https://lanciweb.github.io/demo/api/pictures/')
-    // It converts data in json when fetched
-    .then(response => response.json())
-    // It executes arrow function when data is converted in json format
-    .then(data => {
-        // It iterates for every element in data fetched
-        for (let i = 0; i < data.length; i++) {
-            // Api fetching variables
-            const postData = data[i]
-            const postId = postData.id
-            const postTitle = postData.title.toUpperCase()
-            const postDate = postData.date
-            const postUrl = postData.url
+        // It executes arrow function when data is converted in json format
+        .then(data => {
 
-            // Card markup for posts
-            let postCardMarkup = `
-                <div class="post-card card p-2 pb-3 d-flex gap-2 col">
-                <img class="post-card-pin" src="img/pin.svg" alt="">
-                <img class="post-card-image" src="${postUrl}" alt="Post photo ${postId}">
-                <p class="post-card-date">${postDate}</p>
-                <p class="post-card-title">${postTitle}</p>
-                </div>
-                `
+            // It iterates for every element in data fetched
+            for (let i = 0; i < data.length; i++) {
+                // Api fetching variables assignments
+                postData = data[i]
+                postId = postData.id
+                postTitle = postData.title.toUpperCase()
+                postDate = postData.date
+                postUrl = postData.url
 
-            // It adds the postcard markup to posts container
-            postsContainer.innerHTML += postCardMarkup
-        }
+                // Card markup for posts
+                let postCardMarkup = `
+                    <div class="post-card card p-2 pb-3 d-flex gap-2 col">
+                    <img class="post-card-pin" src="img/pin.svg" alt="">
+                    <img class="post-card-image" src="${postUrl}" alt="Post photo ${postId}">
+                    <p class="post-card-date">${postDate}</p>
+                    <p class="post-card-title">${postTitle}</p>
+                    </div>
+                    `
 
+                // It adds the postcard markup to posts container
+                container.innerHTML += postCardMarkup
+            }
     })
+
     // It logs errors in Api fetching
     .catch(err => console.error(err))
+}
+
+// MAIN
+
+addCardsToContainerFromApi(postsFetchUrl, postsContainer)
